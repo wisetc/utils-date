a4bb3460a78a02312e1773c39411e95d4997a52c
Mark Wang <zhi@uqugu.com>
2020-04-02 15:36:10 +0800

    init project

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import moment from 'moment-timezone';
+
+moment.tz.setDefault('Asia/Shanghai');
+
+export const setDefaultTimezone = moment.tz.setDefault;
+
+/**
+ * 获得时间对应的日期时间，以 localeString 表示
+ */
+export function datetimeStr(
+  date: moment.MomentInput,
+  delimiter: string[] = ['-', ':']
+): string {
+  const [s0, s1] = delimiter;
+  return moment(date).format(
+    `${['YYYY', 'MM', 'DD'].join(s0)} ${['HH', 'mm', 'ss'].join(s1)}`
+  );
+}
+
+/**
+ * 获得时间对应的日期，以 localeString 表示
+ */
+export function dateStr(
+  date: moment.MomentInput,
+  delimiter: string = '-'
+): string {
+  return moment(date).format(['YYYY', 'MM', 'DD'].join(delimiter));
+}
+
+/**
+ * 获取当前时间的时间戳
+ */
+export function now(): number {
+  return moment().valueOf();
+}
+
+/**
+ * 获取当天的时间戳
+ */
+export function today(): number {
+  return moment()
+    .startOf('day')
+    .valueOf();
+}
+
+/**
+ * 获取两个时间的时间间隔，以 localeString 表示
+ */
+export function duration(
+  startTime: moment.MomentInput,
+  endTime: moment.MomentInput
+): string {
+  return dateStr(startTime) + ' 至 ' + dateStr(endTime);
+}
+
+/**
+ * 获得时间对应的时间戳
+ */
+export function valueOf(date: moment.MomentInput): number {
+  return moment(date).valueOf();
+}
+
+/**
+ * 获得时间戳表示的时间对应日期的时间戳
+ */
+export function startOfDay(timestamp: number): number {
+  const date = new Date(timestamp);
+  const y = date.getFullYear();
+  const m = date.getMonth();
+  const d = date.getDate();
+  return new Date(y, m, d).getTime();
+}
+
+/**
+ * 计算得两个时间戳表示的天数间隔 - 两个时间戳需为同一个时区
+ */
+export function deltaDays(timestamp0: number, timestamp1: number): number {
+  const [day0, day1] = [timestamp0, timestamp1].map(startOfDay);
+  const dur = day1 - day0;
+  const oneDay = 24 * 3600 * 1000;
+
+  return Math.floor(dur / oneDay);
+}
+
+export default moment;
---
+++ test/index.test.ts
@@ -0,0 +1,44 @@
+import moment from 'moment';
+import { deltaDays, startOfDay } from '../src';
+
+describe('startOfDay', () => {
+  test('2020-02-01 10:15', () => {
+    const tStr = '2020-02-01 10:15';
+    const t = Date.parse(tStr);
+    expect(startOfDay(t)).toBe(
+      moment(t)
+        .startOf('days')
+        .valueOf()
+    );
+  });
+});
+
+describe('deltaDays', () => {
+  const today = startOfDay(Date.now());
+
+  const oneHour = 3600 * 1000;
+
+  test('now', () => {
+    expect(deltaDays(Date.now(), Date.now())).toBe(0);
+  });
+
+  test('today + 2h, today + 1h', () => {
+    expect(deltaDays(today + 2 * oneHour, today + 1 * oneHour)).toBe(0);
+  });
+
+  test('today + 2h, today + 25h', () => {
+    expect(deltaDays(today + 2 * oneHour, today + 25 * oneHour)).toBe(1);
+  });
+
+  test('today + 2h, today + 49h', () => {
+    expect(deltaDays(today + 2 * oneHour, today + 49 * oneHour)).toBe(2);
+  });
+
+  test('today + 2h, today - 1h', () => {
+    expect(deltaDays(today + 2 * oneHour, today - 1 * oneHour)).toBe(-1);
+  });
+
+  test('today + 2h, today - 47h', () => {
+    expect(deltaDays(today + 2 * oneHour, today - 47 * oneHour)).toBe(-2);
+  });
+});
